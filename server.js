0b8471d6ebbcc42b4c3e691ea466a247ec3eb434
Lina <talmatlina2@gmail.com>
2026-05-06 14:29:18 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // on importe express et les fichiers de routes
-const express = require('express');
-const participantsRoutes = require('./routes/participants');
-const defisRoutes = require('./routes/defis');
+import express from 'express';
+import participantsRoutes from './routes/participants.js';
+import defisRoutes from './routes/defis.js';
 
 const app = express();
 
 // permet de lire le JSON dans le body des requêtes
 app.use(express.json());
 
-// on branche les routes sur leurs d'URL
+// on branche les routes sur leurs URL
 app.use('/participants', participantsRoutes);
 app.use('/defis', defisRoutes);
 
 const PORT = 3000;
 
-// on démarre le serveur
+// on demarre le serveur
 app.listen(PORT, () => {
     console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
